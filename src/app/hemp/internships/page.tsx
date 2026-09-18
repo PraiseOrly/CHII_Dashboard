9fe885a61abd09ee16b2e9d5c926168f99e7189c
PraiseOrly <praiseorly.a@gmail.com>
2026-09-18 09:37:55 +0200

    Fix rendering issue by closing section tag in HEMPInternships component

--- src/app/hemp/internships/page.tsx
+++ src/app/hemp/internships/page.tsx
@@ -766,6 +766,7 @@ export default function HEMPInternships() {
                 </ResponsiveContainer>
               </Panel>
             </div>
+            )}
           </section>
         )}
 
